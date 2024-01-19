dbb42641959ae81beb6bb04b51b848f887c7ed32
Fleppy_Gamer <fleppyg@gmail.com>
2024-01-18 21:30:33 -0300

    Feat: Behavior added to the website - Masks, validation and animations created

--- js/main.js
+++ js/main.js
@@ -2,4 +2,56 @@ $(document).ready(function(){
     $('#hamburger-menu').click(function(){
         $('nav').slideToggle();
     })
+
+    $('.product button').click(function(){
+        const destination = $('#contact');
+
+        $('html').animate({
+            scrollTop: destination.offset().top
+        }, 800);
+
+        const interestedProduct = $(this).parent().find('h3').text();
+
+        $('#travel-package').val(interestedProduct)
+    })
+
+    function phoneMask(element){
+        $(element).mask('(00) 00000-0000');
+    }
+
+    phoneMask('#phone');
+    phoneMask('#second-phone');
+
+    $('form').validate({
+        rules:{
+            name:{
+                required: true,
+            },
+            address:{
+                required: true,
+            },
+            phone:{
+                required: true,
+            },
+            secondPhone:{
+                required: false,
+            },
+            email:{
+                required: true,
+            },
+            travelPackage:{
+                required: true,
+            },
+            message:{
+                required: false,
+            },
+        },
+        messages:{
+           name: 'Este campo é obrigatório',
+           address: 'Este campo é obrigatório',
+           phone: 'Este campo é obrigatório',
+           email: 'Este campo é obrigatório',
+           travelPackage: 'Este campo é obrigatório',
+        },
+    });
 })
